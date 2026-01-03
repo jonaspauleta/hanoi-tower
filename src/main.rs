c9f501fd509f2c092f4e730ad839dcaa8ad3a465
João Paulo Santos <jonaspauleta2@gmail.com>
2026-01-03 10:59:07 +0000

    user input quantity of disks

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
 use hanoi_tower::solve_hanoi;
 
+use std::io;
+
 fn main() {
-    let disks = 3;
+    println!("Enter the number of disks:");
+
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    let disks: u32 = match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Invalid input, defaulting to 3 disks.");
+            3
+        }
+    };
+
     println!("Solving Tower of Hanoi with {} disks...", disks);
 
     let moves = solve_hanoi(disks, "A", "C", "B");
@@ -10,5 +26,4 @@ fn main() {
         println!("Step {}: Move disk {} from {} to {}", i + 1, disk, from, to);
     }
 
-    println!("Solved in {} steps!", moves.len());
-}
+    println!("Solved in {} steps!", moves.len()); }
